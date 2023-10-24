e8967e7021dbace4e24e1b38d938825633215727
Ak-Yadav3122 <aichchhik12@gmail.com>
2023-10-24 14:08:40 +0530

    update some changes

--- 06CurrencyGenerator/src/App.jsx
+++ 06CurrencyGenerator/src/App.jsx
@@ -32,8 +32,8 @@ function App() {
             backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         }}
     >
-        <div id='box' className="w-full m-10">
-            <div className="w-full max-w-md ml-auto mr-32 border border-gray-600 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <div className="w-full m-10">
+            <div className="w-full max-w-md ml-auto mr-32 border border-gray-700 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                 <form
                     onSubmit={(e) => {
                         e.preventDefault();
